b38d0940b37db8e783744a6a31bf028121b82262
andrew-orlov <ao@virtoway.com>
2015-06-04 14:34:42 +0200

    Merge

--- Tests/UI.FunctionalTests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UI.FunctionalTests")]
-[assembly: AssemblyDescription("")]
-
-[assembly: ComVisible(false)]
