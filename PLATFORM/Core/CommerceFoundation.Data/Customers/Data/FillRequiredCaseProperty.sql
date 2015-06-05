d3397bd6d4d81e8434522eafa81a0416889da298
E.Tatarincev <et@virtoway.com>
2015-06-05 10:31:23 +0200

    Remove old projects

--- PLATFORM/Core/CommerceFoundation.Data/Customers/Data/FillRequiredCaseProperty.sql
+++
@@ -1,5 +0,0 @@
-﻿update [Case] 
-set 
-AgentId = (select top 1 [AccountId] FROM [PlatformAccount]),
-AgentName = (select top 1 UserName FROM [PlatformAccount]),
-CaseTemplateId = (select top 1 CaseTemplateId from [CaseTemplate])
