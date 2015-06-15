997db73d9867e094a382af3c7e50d52e03b9045c
Woland2k <sasha@virtoway.com>
2015-06-15 16:56:05 -0700

    load tests

---
+++ STOREFRONT/Tests/Web.LoadTests/Web.LoadTests/PerformanceUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirtoCommerce.ApiClient;
+using VirtoCommerce.ApiClient.Extensions;
+
+namespace Web.LoadTests
+{
+    [TestClass]
+    public class PerformanceUnitTests
+    {
+        [TestMethod]
+        public void LoadAllStores()
+        {
+            for (int index = 0; index < 500; index++)
+            {
+                var client = ClientContext.Clients.CreateStoreClient();
+                var stores = client.GetStoresAsync().Result;
+            }
+            //var stores2 = client.GetStoresAsync().Result;
+        }
+    }
+}
